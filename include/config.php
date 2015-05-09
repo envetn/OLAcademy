05cc67b2fac04d75ca5267b5d97e2844fe17fe3c
Ejdamm <adamgeorgsson@gmail.com>
2015-05-09 17:59:13 +0200

    updated database configuration

--- include/config.php
+++ include/config.php
@@ -2,10 +2,10 @@
 /*GLOBAL VARIABLES*/
 
 $GLOBAL['database']['dsn']            = 'mysql:host=localhost;dbname=olacademy;'; 
-$GLOBAL['database']['username']		= 'root'; // still using default settings
-$GLOBAL['database']['password']		= ''; //hallonsaft
+$GLOBAL['database']['username']		= 'olacademy';
+$GLOBAL['database']['password']		= 'hallonsaft';
 $GLOBAL['database']['driver_option']	= array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'");
 
-$GLOBAL['pageTitle'] = "Ol Academy";
+$GLOBAL['pageTitle'] = "OL-Academy";
 
 ?>
