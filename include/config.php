facbb8752c910f779fed5a0b77ca64b0140d1789
envetn <foten_92@hotmail.com>
2015-05-09 20:11:55 +0200

    changed salt

--- include/config.php
+++ include/config.php
@@ -8,5 +8,5 @@ $GLOBAL['database']['username']	    	= 'olacademy';
 $GLOBAL['database']['password']		    = 'hallonsaft';
 $GLOBAL['database']['driver_option']	= array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'");
 $GLOBAL['pageTitle'] = "OL-Academy";
-$GLOBAL['salt_char'] = "£@$";
+$GLOBAL['salt_char'] = "#@$";
 ?>
--- include/header.php
+++ include/header.php
@@ -1,5 +1,7 @@
 <?php
 include("include/config.php");
+include("include/database.php");
+$db = new Database($GLOBAL['database']);
 ?>
 
 <html>
@@ -8,17 +10,20 @@ include("include/config.php");
     <meta charset="utf-8">
 	<title> <?php echo $GLOBAL['pageTitle']; ?></title>
 	<link rel="stylesheet" type="text/css" href="style/style.css">
+	
+	<div id='div_header'>
+        <nav id='nav_header'>
+            <a href=''>Site1</a>
+            <a href=''>Site2</a>
+            <a href=''>Site3</a>
+            <a href=''>Site4</a>
+            <a href='login.php'>login</a>
+         </nav>
+    <?php include("login.php");?>
+    </div>
 
 </head>
 <body>
 
 
-<div id='div_header'>
-    <nav id='nav_header'>
-        <a href=''>Site1</a>
-        <a href=''>Site2</a>
-        <a href=''>Site3</a>
-        <a href=''>Site4</a>
-        <a href='login.php'>login</a>
-    </nav>
-</div>
+
