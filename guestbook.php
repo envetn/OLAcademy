e093127d0d8f38aef54536d99dc151c4ce6ac155
Ejdamm <adamgeorgsson@gmail.com>
2015-05-10 21:01:19 +0200

    added guestbook and footer

---
+++ guestbook.php
@@ -0,0 +1,20 @@
+<?php
+include("include/header.php");
+$limit  = 15; //Posts per page in guestbook
+$offset = isset($_GET['offset']) ? $_GET['offset'] : 0; //Start index
+
+if (isset($_POST['submit']))
+{
+	makePost($db, $_POST['name'], $_POST['text']);
+}
+
+?>
+<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST"> 
+	<label>Name:<br><input type="text" name="name" size="30"/></label><br>
+	<label>Message:<br><textarea name="text" rows="8" cols="40"></textarea></label><br>
+	<label><input type="submit" name="submit" value="Submit"/></label><br>
+</form>
+
+<?php presentPost($db, $offset); ?>
+
+<?php include("include/footer.php"); ?>
--- include/functions.php
+++ include/functions.php
@@ -3,3 +3,78 @@ function linux_server()
 {
     return in_array(strtolower(PHP_OS), array("linux", "superior operating system"));
 }
+
+
+// Insert post in guestbook
+function makePost($db, $name, $text)
+{
+	// Remove html tags
+	$name = strip_tags($name);
+	$text = strip_tags($text);
+
+	//Make clickable links
+	preg_match_all('/http\:\/\/[\w\d\-\~\^\.\/]{1,}/',$text,$results);
+	foreach($results[0] as $value)
+	{
+		$link = '<a href="' . $value . '" target="_blank">' . $value . '</a>';
+		$text = preg_replace('/' . preg_quote($value,'/') . '/',$link,$text);
+	}
+	//Control message length
+	$max_text_length = 2000;
+	$max_name_length = 50;
+	if (strlen($text) > $max_text_length)
+	{
+		$GLOBALS['error'] = "<pre class=red>Text must not exceed " . $max_text_length . " characters.</pre>";
+	}
+	elseif (strlen($name) > $max_name_length)
+	{
+		$GLOBALS['error'] = "<pre class=red>Name must not exceed " . $max_name_length . " characters.</pre>";
+	}
+	else
+	{
+		// Check if all fields are filled
+		if(empty($name) or empty($text))
+		{
+			$GLOBALS['error'] = "<pre class=red>Fyll i alla fält.</pre>";
+		}
+		else
+		{
+			$sql = "INSERT INTO posts (name, text, date) VALUES(?,?,?)"; //Prepare SQL code
+			$params = array($name, $text, date('Y-m-d H:i:s')); //Prepare query
+			$db->ExecuteQuery($sql, $params, false); //Execute query
+			header('Location: ' . $_SERVER['PHP_SELF']); //Refresh page
+		}
+	}
+}
+
+
+//Present posts from table and prepare paging
+function presentPost($db, $offset, $limit=15) 
+{
+	$sql = "SELECT * FROM posts ORDER BY ID DESC LIMIT $offset, $limit"; //Prepare SQL code
+	$params = array($offset, $limit);//Prepare query
+	$result = $db->queryAndFetch($sql); //Execute query
+
+	//Output data of each row
+	foreach($result as $row)
+	{		
+		$name = $row->name;
+		$text = $row->text;
+		$date = $row->date;
+		$text = nl2br($text); //Insert line breaks where newlines (\n) occur in the string:
+		
+		//Create html code for each row
+		$post = "<div class='post'>
+					<span class='name'>" . $name . " wrote:</span>
+					<span class='date'>" . $date . "</span>
+					<hr>
+					<span class='text'>" . $text . "</span>
+				</div>";
+		echo $post;
+		
+	
+	}
+}
+
+
+?>
