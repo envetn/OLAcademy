f0b05d4b9e7fa1bf4ed0ef39f3c8070dd32d96b6
Lofie <lofielus@gmail.com>
2015-05-29 20:08:59 +0200

    Added page for news and some minor styling

--- include/functions.php
+++ include/functions.php
@@ -75,9 +75,10 @@ function presentPost($db, $offset, $limit)
 	}
 }
 
-
-
-
+function datetime()
+{
+	return date('Y-m-d', time());
+}
 
 function validateText($text)
 {
@@ -98,6 +99,99 @@ function exceptions_error_handler($severity, $message, $filename, $lineno) {
 	}
 }
 
+function getArticleSideBar($db)
+{
+	$privilege = getUserPriviledge($db);
+
+	
+	if(isset($_GET['p']) && is_numeric($_GET['p']))
+	{
+		$nid = $_GET['p'];
+	}
+	else
+	{
+		$nid = 1;
+	}
+	if(isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] >= 0)
+	{
+		$page = $_GET['page'];
+	}
+	else
+	{
+		$page = 0;
+	}
+	$increase = "news.php?page=".($page+5)."&p=".$nid;
+
+	if( ($page-5) > -1)
+	{
+		$decrease = "news.php?page=".($page-5)."&p=".$nid; //add check so 0 is the lowest, and that its impossible to go futher than existing article
+	}
+	else
+	{
+		$decrease = "news.php?page=".($page)."&p=".$nid;
+	}
+	
+	
+	$sql = "SELECT * FROM news ORDER BY added DESC LIMIT $page , 5 ";
+	$params = array($page);
+	$res = $db->queryAndFetch($sql,$params);
+	
+	$side_article = "<article id='side_article'><h4>Nyheter</h4>";
+	//add paging
+	//var_dump(getExtensionOnUrl());
+	
+	
+	$side_article .= "<h4 style='#text-align: center;'>
+					<a href='".$decrease."'><-Prev</a>
+					 &nbsp&nbsp &nbsp&nbsp
+					<a href='".$increase."'>Next-></a></h4>";
+	
+	foreach ($res as $key)
+	{
+		$side_article .= "<section>";
+		$side_article .= "<a href='news.php?page=".$page."&p=".$key->id."'<h3>". $key->title ."</h3>";
+		$side_article .= "<p class='date_p'>". $key->added . "</p>";
+		$side_article .= "<p class='NewsContent_p'>". validateText($key->content) ."</p>";
+		$side_article .= "<p class='NewsBy_p'><b>Av: </b>". $key->author ."</p></a>";
+		if($privilege == 1 || $privilege == 2)
+		{
+			$side_article .= "<a id='article_remove' href='news.php?r=".$key->id."'>&#8649;  Ta bort  &#8647; </a>";
+		}
+		$side_article .= "</section><hr/>";
+	}
+
+	return $side_article .= "</article>";;
+}
+
+function getExtensionOnUrl()
+{
+	try 
+	{
+		preg_match("/\?.*/", $_SERVER['HTTP_REFERER'], $result);
+		$extension = implode($result);
+	}
+	catch(Exception $e)
+	{
+		$extension = $e;
+	}
+	
+	return $extension;
+}
+
+
+function getUserPriviledge($db)
+{
+	if(isset($_SESSION['username']))
+	{
+		$sql = "SELECT Privilege FROM users WHERE id=? LIMIT 1";
+		$params = array($_SESSION['uid']);
+		$res = $db->queryAndFetch($sql, $params);
+		//TODO : is this right?
+		return $res[0]->Privilege;
+	}
+	return 0;
+}
+
 function displayErrorMessage($message)
 {
 	if(preg_match("/parse/", getcwd()))
@@ -114,6 +208,5 @@ function displayErrorMessage($message)
 		<div class='youShallNotPassDivP'><p>The Wizard says: </p> <p style='color:red'>$message </p></div>
 		</div>";
 	}
-
 }
 ?>
--- news.php
+++ news.php
@@ -1,92 +1,102 @@
+<script type="text/javascript">
+<!--
+
+//-->
+
+    
+</script>
 <?php
+$pageTitle = "- Nyheter";
 include("include/header.php");
 
-$sql = "SELECT * FROM news";
-$res = $db->queryAndFetch($sql);
-$listOfArticle[0] = "";
-$latestArticle = 0;
-$side_article = "<article id='side_article'><h4>Nyheter</h4>";
-foreach ($res as $key)
-{
-	//sidebar
-	$side_article .= "<section>";
-	$side_article .= "<a href='news.php?p=".$key->id."'<h3>". $key->title ."</h3>";
-	$side_article .= "<p class='date_p'>". $key->added . "</p>";
-	$side_article .= "<p>". validateText($key->content) ."</p>";
-	$side_article .= "<p><b>Av: </b>". $key->author ."</p></a>";
-	$side_article .= "</section><hr/>";
-	
-	//main
-	//might be to much if there are many news
-	//TODO: Stupid idea, the page is reloaded everytime a article is clicked. Better call db everytime a article is clicked.
-	$listOfArticle[$key->id]['title']     = $key->title;
-	$listOfArticle[$key->id]['content']   = $key->content;
-	$listOfArticle[$key->id]['Author']	  = "";
-	$listOfArticle[$key->id]['date'] 	  = "";
-	
-	$latest = $key->id;//not a very good solution
-	
-	
-}
-$side_article .= "</article>";
+$priviledge = getUserPriviledge($db);
+$btn_addNew = ($priviledge == 1 || $priviledge == 2) ? "<form method='get'><input type='submit' value='Lägg till' name='p'/></form>" : "<p>Logga in för att lägga till nyheter.</p>";
 
-if(isset($_GET['p']) && is_numeric($_GET['p']))
+if (isset($_GET['p']) && $_GET['p'] == "Lägg till")
 {
-	$p = $_GET['p'];
-	try 
+	/*Add news form */
+	if($priviledge == 1 || $priviledge == 2)
 	{
-		$singleArticle = "<article id='singeArticle'><h3 style=''>" . $listOfArticle[$p]['title'] ."</h3>";
-		$singleArticle .= "<p>".  $listOfArticle[$p]['content'] ."</p>";
-		$singleArticle .= "</article>";
+		$singleArticle = "<form id='form_addNew' method='post'>
+		<input name='title' placeholder='Title' type='text'/><br/>
+		<textarea name='content' placeholder='Innehåll' type='text' cols='50' rows='5'></textarea><br/>
+		<input name='author' placeholder='Av' type='text'/><br/>
+		<input type='submit' name='btn_addNews' value='Lägg till'/>
+		</form>";
 	}
-	catch (Exception $e) 
-	{
-		$singleArticle = "<article id='singeArticle'><h3> Ingen nyhet hittades</h3></article>";
-	}
+	
 }
-else
+else if(isset($_GET['r']) && is_numeric($_GET['r']) && $priviledge == 2)
 {
-	$singleArticle = "<article id='singeArticle'><h3 style=''>" . $listOfArticle[$latestArticle + 1]['title'] ."</h3>";
-	$singleArticle .= "<p>".  $listOfArticle[$latestArticle + 1]['content'] ."</p>";
-	$singleArticle .= "</article>";
+	/*remove news */
+	// TODO : display popup before delete?
+	$id = $_GET['r'];
+	$sql = "DELETE FROM news WHERE id=?";
+	$params = array($id);
+	$db->ExecuteQuery($sql, $params);
+	header("Location: news.php");
 }
-
-
 
 if(isset($_POST['btn_addNews']))
 {
 	if($_POST['title'] != "" && $_POST['content'] != "" && $_POST['author'] != "" )
 	{
-		//Gather content and send to database
+		if($priviledge == 1 || $priviledge == 2)
+		{
+			$title 	     = strip_tags($_POST['title']);
+			$content	 = strip_tags($_POST['content']);
+			$author      = strip_tags($_POST['author']);
+			$date		 = datetime();
+			$sql = "INSERT INTO news (title, content, author, added) VALUES (?,?,?,?)";
+			$params = array($title, $content, $author, $date);
+			
+			$db->ExecuteQuery($sql, $params);
+			header("Location: news.php");
+		}
 	}
 }
+
+if(isset($_GET['p']) && is_numeric($_GET['p']))
+{
+	//check page
+	if(isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] >= 0)
+	{
+		$page = $_GET['page'];
+	}
+	else
+	{
+		$page = 0;
+	}
+	$p = $_GET['p'];
+
+	$sql = "SELECT * FROM news WHERE id=? LIMIT 1";
+	$params = array($p);
+	$res = $db->queryAndFetch($sql,$params);
+
+}
+else
+{
+	$sql = "SELECT * from news ORDER BY id DESC LIMIT 1;";
+	$res = $db->queryAndFetch($sql);
+}
+try
+{
+	$singleArticle = "<article id='singeArticle'><h3 style=''>" . $res[0]->title ."</h3><hr style='width:80%;'/>";
+	$singleArticle .= "<p>".   $res[0]->content ."</p>";
+	$singleArticle .= "<p>By: ".   $res[0]->author ."</p>";
+	$singleArticle .= "</article>";
+}
+catch (Exception $e)
+{
+
+	$singleArticle = "<article id='singeArticle'><h3> Ingen nyhet hittades <hr/><br/> ".$e."</h3></article>";
+}
+
 ?>
-<style>
-#form_addNew
-{
-	border:1px solid black;
-	float:right;
-	width:60%;
-	margin-top:4%;
-	margin-right:4%;
-	padding:2%;
-}
-#form_addNew input, textarea
-{
-	width:90%;
-}
 
-</style>
 <div id='div_articles'>
-<?php echo isset($side_article)  ?  $side_article  : "";?>
+<?php echo $btn_addNew; ?>
+<?php echo getArticleSideBar($db);?>
 <?php  echo isset($singleArticle) ?  $singleArticle : "";?>
-<form id='form_addNew' method='post'>
-	<input name='title' placeholder='Title' type='text'/><br/>
-	<textarea name='content' placeholder='Innehåll' type='text' cols='50' rows='5'></textarea><br/>
-	<input name='author' placeholder='Av' type='text'/><br/>
-	<input type='submit' name='btn_addNews' value='Lägg till'/>
-</form>
 </div>
-
-
 </body>
